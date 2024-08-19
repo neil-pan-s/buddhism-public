937327d8b59428f2c0ad1938d5429ec388a36252
Neil Pan <neil.pan@qq.com>
2024-08-19 20:44:25 +0800

    : )

---
+++ assets/Home-Bakx_FEE.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-eIZUS2hj.js";const s={},t={class:"home"};function n(r,_){return o(),c("div",t)}const f=e(s,[["render",n]]);export{f as default};
--- assets/Mantra-BUOsjKIC.js
+++ assets/Mantra-BUOsjKIC.js
@@ -1,4 +1,4 @@
-import{a as n,o as s,f as i}from"./index-CSBJWnQd.js";const p={__name:"Mantra",setup(e){const a=[{title:"常颂经咒-序言",text:`
+import{a as n,o as s,f as i}from"./index-eIZUS2hj.js";const p={__name:"Mantra",setup(e){const a=[{title:"常颂经咒-序言",text:`
     <center style="font-size: 16px">
       <img src="/icons/favicon.svg" style="width: 128px;" />
       <h3>佛经与咒言  从本质上说  都是佛教的修行法门</h3>其目的都是 <span class="highlight">教世人如何 出离世间苦海  成就佛道</span> 
